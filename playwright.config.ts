ad9095400684f0c3ae401f84da1c6a9cf4ef9da0
rezaparamarta <rezaparamarta30@gmail.com>
2026-02-11 00:48:49 +0700

    yu ah

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,22 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  use: {
-    baseURL: process.env.BASE_URL || 'https://www.saucedemo.com',
-    headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-  },
-
   reporter: [
     ['list'],
-    ['playwright-qase-reporter', {
-      apiToken: process.env.QASE_API_TOKEN,
-      projectCode: process.env.QASE_PROJECT_CODE,
-      run: {
-        complete: true,
+    [
+      'playwright-qase-reporter',
+      {
+        mode: 'testops',
+        testops: {
+          api: {
+            token: process.env.QASE_TESTOPS_API_TOKEN,
+          },
+          project: process.env.QASE_TESTOPS_PROJECT,
+          run: {
+            complete: true,
+          },
+        },
       },
-    }],
+    ],
   ],
 });
-
-console.log('QASE_API_TOKEN:', !!process.env.QASE_API_TOKEN);
-console.log('QASE_PROJECT_CODE:', process.env.QASE_PROJECT_CODE);
-
