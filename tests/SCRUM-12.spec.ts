50e658646e2b9c560a5a1a2d0ce35b047db24744
Reza Paramarta <rparamarta7@gmail.com>
2026-02-11 00:37:00 +0700

    test: add  from Jira

---
+++ tests/SCRUM-12.spec.ts
@@ -0,0 +1,24 @@
+
+import { test, expect } from '@playwright/test';
+
+test('SCRUM-12 - [NEGATIVE] Login fails with invalid password', async ({ page }) => {
+  test.info().annotations.push({
+    type: 'qase',
+    description: 'NLFWIP-1'
+  });
+
+  
+  await page.goto('/');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'wrong_password');
+  await page.click('#login-button');
+
+  const errorMessage = page.locator('[data-test="error"]');
+
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText(
+    'Epic sadface: Username and password do not match any user in this service'
+  );
+  
+});
